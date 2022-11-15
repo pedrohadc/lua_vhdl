aa0ad86f67932b2be61d654c62f703614b5b4da5
Pedro Ananias <105325542+pedrohadc@users.noreply.github.com>
2022-11-15 11:56:19 -0300

    Update subtracao.vhd

--- subtracao.vhd
+++ subtracao.vhd
@@ -46,67 +46,26 @@ entity subtracao is
 
 	end subtracao;
 	
-
-	
-
-
-
 	
 
        	architecture hardware of subtracao is
-	
-
-	
-
-
-
-	
 
 	 component somador1bit                         -- 'buscando' somador de 1 bit
-	
-
-	 port (a, b, cin : in std_logic;               -- entradas e carry in do somador
-	
-
+		
+	 port (a, b, cin : in std_logic;               -- entradas e carry in do somador	
 	       c, cout : out std_logic);               -- soma e carry out do somador
 	
-
 	 end component;
-	
-
-	
-
-
-
 	
 
 	 signal v: std_logic_vector(3 downto 1);
 	
-
-	
-
-	
-
 	--para fazer subtração, faremos o complemento a 2 com o vetor y, e para isso, na subtração
 	
-
 	-- inverteremos cada bit de y e o primeiro carry in será 1
-	
-
-	
-
-
-
-	
+		
 
 	begin
-	
-
-	
-
-
-
-	
 
 	 soma0 : somador1bit PORT MAP(  x(0), not(y(0)), '1', s(0),  v(1));
 	
@@ -118,8 +77,6 @@ entity subtracao is
 	
 
 	 soma3 : somador1bit PORT MAP(  x(3), not(y(3)), v(3), s(3), carryout);
-	
-
 
 	end hardware;
 	
