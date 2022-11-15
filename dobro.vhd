7879b76486b63c3627096bf94eb3bfd798eaf555
Pedro Ananias <105325542+pedrohadc@users.noreply.github.com>
2022-11-15 11:52:45 -0300

    Update dobro.vhd

--- dobro.vhd
+++ dobro.vhd
@@ -31,142 +31,35 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity dobro is
 	
-
-	
-
-	
-
 	port (x: in std_logic_vector(3 downto 0);     --vetores de entrada
-	
-
-	
-
-	
-
 	     carryin : in std_logic;                    --carry in geral
-	
-
-	
-
-	
-
 	     s : out std_logic_vector(3 downto 0);      --soma
-	
-
-	
-
-	
-
 	     carryout : out std_logic);                  --carry out geral
 	
-
-	
-
-	
-
 	end dobro;
 	
 
-	
-
-	
-
-	
-
-
-
-	
-
-	
-
-	
-
 	architecture hardware of dobro is
-	
 
-	
-
-	
-
-	
-
-
-
-	
-
-	
-
-	
-
-	 component somador1bit                         -- 'buscando' somador de 1 bit
-	
-
-	
-
-	
-
+		
+		
+	 component somador1bit                         -- 'buscando' somador de 1 bit\
+		
 	 port (a, b, cin : in std_logic;               -- entradas e carry in do somador
-	
-
-	
-
-	
-
 	       c, cout : out std_logic);               -- soma e carry out do somador
-	
-
-	
-
-	
-
+		
 	 end component;
-	
-
-	
-
-	
-
-	
-
-
-
-	
-
-	
-
 	
 
 	 signal v: std_logic_vector(3 downto 1);
 	
 
-	
-
-	
-
-	
-
-
-
-	
-
-	
-
-	
-
 	begin
 	
-
-	
-
-	
-
 	 soma0 : somador1bit PORT MAP(  x(0), x(0) , '0', s(0),  v(1));
-
-	
 
 	 soma1 : somador1bit PORT MAP(  x(1), x(1) , v(1), s(1), v(2));
 	
-
 	 soma2 : somador1bit PORT MAP(  x(2), x(2) , v(2), s(2), v(3));
 	
 	 soma3 : somador1bit PORT MAP(  x(3), x(3) , v(3), s(3), carryout);
